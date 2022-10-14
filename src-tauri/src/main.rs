f98b1a25ee6b9dbe6b3820558cfa24dc1da06240
Michael Wooley <wm.wooley@gmail.com>
2022-10-13 22:19:13 -0400

    tweak ci

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -16,13 +16,3 @@ fn main() {
         .expect("error while running tauri application");
 }
 
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    // use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(1 + 1, 2);
-    }
-}
